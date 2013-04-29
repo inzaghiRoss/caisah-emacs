8d097148f54be17b9db4b6d0646377fec448fb3b
Vlad Piersec <vlad.piersec@gmail.com>
2013-04-29 17:12:43 +0300

    Remove emacs.d

--- .emacs.d/elpa/flycheck-20130415.1715/flycheck-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "flycheck" "20130415.1715" "On-the-fly syntax checking (Flymake done right) [source: github]"
-  '((s "1.3.1")
-    (dash "1.1")
-    (cl-lib "0.1")
-    (emacs "24.1")))
-
